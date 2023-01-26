32be31d96720f644b0572350b89ef2056db2c295
alexundern <novicov7@gmail.com>
2023-01-26 11:11:04 +0200

    add subtraction and division functionality

--- src/app/Main.java
+++ src/app/Main.java
@@ -4,13 +4,17 @@ public class Main {
 
     public static void main(String[] args) {
         System.out.println("My app for Users!");
-        System.out.println("Version 1.0");
+        System.out.println("Version 2.0");
         int a = 8;
         int b = 2;
         int c = add(a, b);
         System.out.println(c);
         int d = multiply(a, b);
         System.out.println(d);
+        int e = minus(a, b);
+        System.out.println(e);
+        int f = divide(a, b);
+        System.out.println(f);
     }
 
     private static int add(int a, int b) {
@@ -19,5 +23,13 @@ public class Main {
 
     private static int multiply(int a, int b) {
         return a * b;
+    }
+
+    private static int minus(int a, int b) {
+        return a - b;
+    }
+
+    private static int divide(int a, int b) {
+        return a / b;
     }
 }
